21b8abd4c35ab12ef2463ec64727d798d86fddcc
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-01 01:06:29 +0900

    2017-08-01, H, 액션 정의 추가

--- image/admin.py
+++ image/admin.py
@@ -4,9 +4,21 @@ from .models import Image
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image_path', 'abs_size', 'label_name', 'tags']
+    list_display = ['id', 'image_path', 'status', 'abs_size', 'label_name', 'tags'] # display 리스트 지정
+    actions = ['make_published', 'make_draft']  # 액션 리스트 지정
 
     def abs_size(self, image):
         return '{} 글자'.format(len(image.abs))
-    abs_size.short_description = 'SIZE'
+    abs_size.short_description = 'SIZE' # 보여질 이름 재정의
+
+    def make_published(self, request, queryset):    # 액션 정의 requst와 querset을 인자로 받음
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 Published 상태로 변경'.format(updated_count))
+    make_published.short_description = '지정 포스팅을 Published 상태로 변경'
+
+    def make_draft(self, request, queryset):    # 액션 정의 requst와 querset을 인자로 받음
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 Draft 상태로 변경'.format(updated_count))
+    make_draft.short_description = '지정 포스팅을 Draft 상태로 변경'
+
 # admin.site.register(Image)
--- image/models.py
+++ image/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class Image(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+
     image_path = models.CharField(max_length=255, unique=True)
     upload_time = models.DateTimeField(auto_now_add=True)
     label_name = models.CharField(max_length=255)
     tags = models.CharField(max_length=100, blank=True)
     abs = models.CharField(max_length=10)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
