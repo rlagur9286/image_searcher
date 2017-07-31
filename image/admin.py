efe2d68445c71cc712d3b3ab971527765e2c628a
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-01 00:50:59 +0900

    2017-08-01, H, admin shortcut

--- image/admin.py
+++ image/admin.py
@@ -4,6 +4,9 @@ from .models import Image
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image_path', 'label_name', 'tags']
+    list_display = ['id', 'image_path', 'abs_size', 'label_name', 'tags']
 
+    def abs_size(self, image):
+        return '{} 글자'.format(len(image.abs))
+    abs_size.short_description = 'SIZE'
 # admin.site.register(Image)
