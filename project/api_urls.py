34cea84c439fb2689e6e6709733a6ed263018523
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-08-21 13:42:21 +0900

    2017-08-21, H, API 문서 url 수정

--- project/api_urls.py
+++ project/api_urls.py
@@ -11,6 +11,7 @@ urlpatterns = [     # 순차적으로 검사됨
     url(r'^create/$', apis.create_project, name='create_project'),
     url(r'^retrieve/$', apis.retrieve_project, name='retrieve_project'),
     url(r'^delete/$', apis.delete_project, name='delete_project'),
+
     url(r'^create/(?P<p_id>\d+)/$', apis.create_label, name='create_label'),
     url(r'^retrieve/(?P<p_id>\d+)/$', apis.retrieve_label, name='retrieve_label'),
     url(r'^delete/(?P<p_id>\d+)/$', apis.delete_label, name='delete_label'),
